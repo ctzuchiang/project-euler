9c1c2a85d2e019510c05b4510f4563c4e935e522
joseph.chao <joseph.chao@titansoft.com.sg>
2017-09-21 20:47:31 +0800

    implement E67_AddBinary

---
+++ LeetCode/Solution/Solution/Easy/051_100/E67_AddBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Solution.Easy._051_100
+{
+    public class E67_AddBinary
+    {
+        public string AddBinary(string binaryStr1, string binaryStr2)
+        {
+            StringBuilder sb = new StringBuilder();
+            int carry = 0;
+            for (int i = binaryStr1.Length - 1, j = binaryStr2.Length - 1; i >= 0 || j >= 0; i--, j--)
+            {
+                int total = ((i >= 0 ? binaryStr1[i] - '0' : 0) + (j >= 0 ? binaryStr2[j] - '0' : 0)) + carry;
+                sb.Insert(0, total == 2 || total == 0 ? '0' : '1');
+                carry = total > 1 ? 1 : 0;
+            }
+            return carry == 1 ? sb.Insert(0, 1).ToString() : sb.ToString();
+
+            //var num1 = Convert.ToInt64(a, 2);
+            //var num2 = Convert.ToInt64(b, 2);
+
+            //return Convert.ToString(num1 + num2, 2);
+        }
+    }
+}
---
+++ LeetCode/Solution/SolutionTests/Easy/051_100/E67_AddBinaryTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Solution.Easy._051_100;
+
+namespace SolutionTests.Easy._051_100
+{
+    [TestFixture()]
+    public class E67_AddBinaryTests
+    {
+        private E67_AddBinary _Target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Target = new E67_AddBinary();
+        }
+
+        [TestCase("0", "0", ExpectedResult = "0")]
+        [TestCase("11", "1", ExpectedResult = "100")]
+        [TestCase("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+            "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011",
+            ExpectedResult = "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000")]
+        public string AddBinaryTests(string binaryStr1, string binaryStr2)
+        {
+            return _Target.AddBinary(binaryStr1, binaryStr2);
+        }
+    }
+}
