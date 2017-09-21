4a95553936e340542e642bc5ce83c2cadbeeb379
joseph.chao <joseph.chao@titansoft.com.sg>
2017-09-21 20:55:45 +0800

    Implement E69_SqrtX

---
+++ LeetCode/Solution/Solution/Easy/051_100/E69_SqrtX.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solution.Easy._051_100
+{
+    public class E69_SqrtX
+    {
+        public int MySqrt(int x)
+        {
+            return (int)Math.Sqrt(x);
+        }
+    }
+}
---
+++ LeetCode/Solution/SolutionTests/Easy/051_100/E69_SqrtXTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Solution.Easy._051_100;
+
+namespace SolutionTests.Easy._051_100
+{
+    [TestFixture()]
+    public class E69_SqrtXTests
+    {
+        private E69_SqrtX _Target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Target = new E69_SqrtX();
+        }
+
+        [TestCase(0, ExpectedResult = 0)]
+        public int MySqrtTests(int number)
+        {
+            return _Target.MySqrt(number);
+        }
+    }
+}
