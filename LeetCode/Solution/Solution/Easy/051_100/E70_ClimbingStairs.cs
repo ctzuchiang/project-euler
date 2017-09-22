96a82635e8ad727049319fcf407aa35475c7d645
joseph.chao <joseph.chao@titansoft.com.sg>
2017-09-22 08:37:16 +0800

    E70_ClimbingStairs

---
+++ LeetCode/Solution/Solution/Easy/051_100/E70_ClimbingStairs.cs
@@ -0,0 +1,13 @@
+﻿namespace Solution.Easy._051_100
+{
+    public class E70_ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int a = 1, b = 1;
+            while (n-- > 0)
+                a = (b += a) - a;
+            return a;
+        }
+    }
+}
---
+++ LeetCode/Solution/SolutionTests/Easy/051_100/E70_ClimbingStairsTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Solution.Easy._051_100;
+
+namespace SolutionTests.Easy._051_100
+{
+    [TestFixture()]
+    public class E70_ClimbingStairsTests
+    {
+        private E70_ClimbingStairs _Target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Target = new E70_ClimbingStairs();
+        }
+
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 3)]
+        [TestCase(4, ExpectedResult = 5)]
+        [TestCase(5, ExpectedResult = 8)]
+        public int ClimbStairsTests(int steps)
+        {
+            return _Target.ClimbStairs(steps);
+        }
+    }
+}
