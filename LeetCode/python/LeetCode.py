40ba88678d508a957220f884165019f4fc73604e
Joseph Chao <joseph.chao@user.noreply.github.com>
2018-06-24 10:31:06 +0800

    bug fix

--- LeetCode/python/LeetCode.py
+++ LeetCode/python/LeetCode.py
@@ -6,9 +6,10 @@ class Solution:
         :rtype: List[int]
         """
         
-        for i in range(0,len(nums)-1):
-            for j in range(i+1,len(nums)-1):
-                if nums[i] + nums[j] == target:
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
                     return list([i,j])
 
         return list([0,0])
