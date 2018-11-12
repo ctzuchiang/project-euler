a349b1da6175630eccf08700222cb52fc171d0a1
Joseph Chao <joseph.chao@user.noreply.github.com>
2018-11-13 00:19:34 +0800

    update

---
+++ LeetCode/Solution/Solution/Others/URLify.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Others
+{
+    public class URLify
+    {
+        public string ReplaceSpace(string str, int trueLength)
+        {
+            string realStr = str.Substring(0, trueLength);
+            string result = string.Empty;
+
+            foreach (var s in realStr)
+            {
+                if (s == ' ')
+                {
+                    result += "%20";
+                    continue;
+                }
+                result += s;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Solution/SolutionTests/Easy/Others/URLifyTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Solution.Others;
+
+namespace SolutionTests.Easy.Others
+{
+    [TestFixture]
+    public class URLifyTests
+    {
+        [TestCase("Hello World   ", 11, ExpectedResult = "Hello%20World")]
+        [TestCase("Hello  World   ", 12, ExpectedResult = "Hello%20%20World")]
+        [TestCase("HelloWorld   ", 12, ExpectedResult = "HelloWorld%20%20")]
+        [TestCase(" Hello World", 12, ExpectedResult = "%20Hello%20World")]
+        [TestCase("Hello World  ", 9, ExpectedResult = "Hello%20Wor")]
+        public string ReplaceSpace_Tests(string input, int trueLength)
+        {
+            var target = new URLify();
+            var actual = target.ReplaceSpace(input, trueLength);
+
+            return actual;
+        }
+    }
+}
