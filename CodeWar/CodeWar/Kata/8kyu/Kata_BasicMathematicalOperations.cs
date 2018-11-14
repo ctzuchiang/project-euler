ca3d37eba31544487c26819787a9513b80da93f2
Joseph Chao <joseph.chao@user.noreply.github.com>
2018-11-15 07:26:27 +0800

    update

---
+++ CodeWar/CodeWar/Kata/8kyu/Kata_BasicMathematicalOperations.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWar.Kata._8kyu
+{
+    public class Kata_BasicMathematicalOperations
+    {
+        public double basicOp(char operation, double value1, double value2)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return value1 + value2;
+                case '-':
+                    return value1 - value2;
+                case '*':
+                    return value1 * value2;
+                case '/':
+                    return value1 / value2;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CodeWar/CodeWarTests/Kata/8kyu/Kata_BasicMathematicalOperationsTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWar.Kata._8kyu;
+using NUnit.Framework;
+
+namespace CodeWarTests.Kata._8kyu
+{
+    [TestFixture()]
+    public class Kata_BasicMathematicalOperationsTests
+    {
+        [TestCase('+', 4, 7, ExpectedResult = 11)]
+        [TestCase('-', 15, 18, ExpectedResult = -3)]
+        [TestCase('*', 5, 5, ExpectedResult = 25)]
+        [TestCase('/', 49, 7, ExpectedResult = 7)]
+        public double basicOpTest(char operation, double value1, double value2)
+        {
+            var target = new Kata_BasicMathematicalOperations();
+
+            var actual = target.basicOp(operation, value1, value2);
+
+            return actual;
+        }
+    }
+}
