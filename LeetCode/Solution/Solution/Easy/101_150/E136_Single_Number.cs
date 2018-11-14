3c31d2e75e47cca91a89b0bfa7402cadc47de69a
Joseph Chao <joseph.chao@jkos.com>
2018-11-14 20:55:10 +0800

    update

--- LeetCode/Solution/Solution/Easy/101_150/E136_Single_Number.cs
+++ LeetCode/Solution/Solution/Easy/101_150/E136_Single_Number.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using System.Collections;
-
-namespace Solution.Easy._101_150
+﻿namespace Solution.Easy._101_150
 {
     public class E136_Single_Number
     {
         public int SingleNumber(int[] nums)
         {
-            var ht = new Hashtable();
+            int a = 0;
             foreach (int n in nums)
             {
-                if (ht.Contains(n))
-                {
-                    ht[n] = (int)ht[n] + 1;
-                }
-                else
-                {
-                    ht.Add(n, 1);
-                }
+                a ^= n;
             }
 
-            foreach (DictionaryEntry de in ht)
-            {
-                if ((int)de.Value == 1)
-                {
-                    return (int) de.Key;
-                }
-            }
-            throw new ArgumentException("Not found");
+            return a;
         }
     }
 }
