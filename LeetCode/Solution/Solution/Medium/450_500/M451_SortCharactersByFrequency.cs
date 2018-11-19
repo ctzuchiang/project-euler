20cef67947a3359f6593600c6085286f4cf54721
Joseph Chao <joseph.chao@jkos.com>
2018-11-19 23:23:21 +0800

    Implement M451_SortCharactersByFrequency

---
+++ LeetCode/Solution/Solution/Medium/450_500/M451_SortCharactersByFrequency.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Solution.Medium._450_500
+{
+    public class M451_SortCharactersByFrequency
+    {
+        public string FrequencySort(string s)
+        {
+            var ht = new Hashtable();
+
+            foreach (var c in s)
+            {
+                if (!ht.Contains(c))
+                {
+                    ht.Add(c, 1);
+                }
+                else
+                {
+                    ht[c] = (int)ht[c] + 1;
+                }
+            }
+
+            string result = string.Empty;
+
+            var len = s.Length - ht.Count + 1;
+            while (len > 0)
+            {
+                foreach (DictionaryEntry hs in ht)
+                {
+                    if ((int)hs.Value == len)
+                    {
+                        result += new string((char)hs.Key, len);
+                    }
+                }
+
+                len--;
+            }
+            return result;
+        }
+
+        public string FrequencySort2(string s)
+        {
+            return new string(s
+                .GroupBy(c => c)
+                .Select(g => new { Char = g.Key, Count = g.Count() })
+                .OrderByDescending(pair => pair.Count)
+                .SelectMany(pair => Enumerable.Range(0, pair.Count).Select(c => pair.Char))
+                .ToArray()
+            );
+        }
+    }
+}
---
+++ LeetCode/Solution/SolutionTests/Medium/450_500/M451_SortCharactersByFrequencyTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Solution.Medium._450_500;
+using System;
+using System.Diagnostics;
+
+namespace SolutionTests.Medium._450_500
+{
+    [TestFixture()]
+    public class M451_SortCharactersByFrequencyTests
+    {
+        private string _inputStr;
+        private string _expected;
+        private Stopwatch _stopwatch;
+
+        private delegate void FrequencySortRunner();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _inputStr = "aaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbccccccccccccccccccddddddddddddddddddd";
+            _expected = "bbbbbbbbbbbbbbbbbbbbbdddddddddddddddddddccccccccccccccccccaaaaaaaaaaaaaaa";
+
+            _stopwatch = new Stopwatch();
+        }
+
+        [Test]
+        public void FrequencySort_Success_Test()
+        {
+            var target = new M451_SortCharactersByFrequency();
+
+            var actual = target.FrequencySort(_inputStr);
+
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void FrequencySort_Success2_Test()
+        {
+            var target = new M451_SortCharactersByFrequency();
+
+            var actual = target.FrequencySort2(_inputStr);
+
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void FrequencySort_Time_running()
+        {
+            Timer(FrequencySort_Success_Test);
+            Timer(FrequencySort_Success2_Test);
+        }
+
+        private void Timer(FrequencySortRunner runner)
+        {
+            _stopwatch.Restart();
+            runner();
+            _stopwatch.Stop();
+            Console.WriteLine(_stopwatch.Elapsed);
+        }
+    }
+}
