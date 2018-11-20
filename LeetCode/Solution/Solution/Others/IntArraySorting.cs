7c826e11bcd564be48c9be2702dcef27d6d7a45e
Joseph Chao <joseph.chao@user.noreply.github.com>
2018-11-20 23:11:46 +0800

    implement ForLoopSorting

--- LeetCode/Solution/Solution/Others/IntArraySorting.cs
+++ LeetCode/Solution/Solution/Others/IntArraySorting.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Solution.Others
 {
@@ -6,7 +8,23 @@ namespace Solution.Others
     {
         public int[] ForLoopSorting(int[] nums)
         {
-            throw new NotImplementedException();
+            int a;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        a = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = a;
+                    }
+                    
+                }
+            }
+
+            return nums;
         }
     }
 }
