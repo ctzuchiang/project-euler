b6b87e9b85729e5065c7fc8fcd9ff065a9850b5a
Joseph Chao <joseph.chao@jkos.com>
2018-12-12 19:30:00 +0800

    update

--- FixPolicyAndUpdate/FixPolicyAndUpdate/Program.cs
+++ FixPolicyAndUpdate/FixPolicyAndUpdate/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace FixPolicyAndUpdate
 {
@@ -12,8 +15,26 @@ namespace FixPolicyAndUpdate
 
             ResetGroupPolicy();
 
+            UpdateGroupPolicy();
+            
             Console.WriteLine("Press any key to exist.");
             Console.ReadLine();
+        }
+
+        private static void UpdateGroupPolicy()
+        {
+            FileInfo execFile = new FileInfo("gpupdate.exe");
+            Process proc = new Process();
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.FileName = execFile.Name;
+            proc.StartInfo.Arguments = "/force";
+            proc.Start();
+            //Wait for GPUpdate to finish
+            while (!proc.HasExited)
+            {
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Update group policy done.");
         }
 
         private static void ResetGroupPolicy()
